f4c8ad4669ba4c10adb7af43f4c2a930fa2aadc7
Menmery <2053186460@qq.com>
2025-11-04 09:40:41 +0800

    二级导航面包屑

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,13 @@ export const getTopCategoryAPI = (id) => {
     }
   })
 }
+
+// 获取二级面包屑导航数据
+export const getCategoryFilterAPI = (id) => {
+  return http({
+    url: '/category/sub/filter',
+    params: {
+      id
+    }
+  })
+}
