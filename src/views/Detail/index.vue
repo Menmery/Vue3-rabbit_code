a42c054300fae6e2817ebf593eec1c58994b5cfc
Menmery <2053186460@qq.com>
2025-11-06 11:04:48 +0800

    放大镜效果实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -27,7 +27,7 @@ onMounted(() => getGoods())
           </el-breadcrumb-item>
           <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{
             goods.categories[0].name
-          }}
+            }}
           </el-breadcrumb-item>
           <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
         </el-breadcrumb>
@@ -38,7 +38,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView></ImageView>
+              <ImageView :imageList="goods.mainPictures"></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
