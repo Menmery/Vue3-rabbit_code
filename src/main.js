d7df2adea5062c39d491a3e9f423ae76dbe481bc
Menmery <2053186460@qq.com>
2025-11-06 20:54:36 +0800

    pinia持久化

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import { lazyPlugin } from './directives'
 // 全局组件插件
 import { componentPlugin } from '@/components/index'
 
+// pinia持久化插件
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import router from './router'
 
@@ -14,8 +17,10 @@ import router from './router'
 import '@/styles/common.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedState)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
