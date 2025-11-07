d7ab1be86876b26cef7af3242016e27bd6d8e333
Menmery <2053186460@qq.com>
2025-11-07 16:46:50 +0800

    购物车总数量和总价格

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 export const useCartStore = defineStore('cart', () => {
   // 购物车数据
@@ -21,10 +21,16 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
   }
 
+  // 计算属性
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
   return {
     cartList,
     addCart,
-    delCart
+    delCart,
+    allCount,
+    allPrice,
   }
 }, {
   persist: true,
