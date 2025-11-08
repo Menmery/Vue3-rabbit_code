77a67c10eb7ad0ddf8a963668cbde455151c9959
Menmery <2053186460@qq.com>
2025-11-08 15:38:40 +0800

    合并未登陆的购物车列表

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -85,7 +85,8 @@ export const useCartStore = defineStore('cart', () => {
     allCheck,
     selectedCount,
     selectedPrice,
-    clearCart
+    clearCart,
+    getNewCartList,
   }
 }, {
   persist: true,
