8ba6dc2fbc059881f3599743680439720cd5df87
Menmery <2053186460@qq.com>
2025-11-10 09:31:11 +0800

    支付结果

--- src/views/Pay/PayBack.vue
+++ src/views/Pay/PayBack.vue
@@ -19,8 +19,8 @@ onMounted(() => getOrderInfo())
     <div class="container">
       <!-- 支付结果 -->
       <div class="pay-result">
-        <span class="iconfont icon-queren2 green"></span>
-        <span class="iconfont icon-shanchu red"></span>
+        <span class="iconfont icon-queren2 green" v-if="$route.query.payResult === 'true'"></span>
+        <span class="iconfont icon-shanchu red" v-else></span>
         <p class="tit">支付{{ $route.query.payResult === 'true' ? '成功' : '失败' }}</p>
         <p class="tip">我们将尽快为您发货，收货期间请保持手机畅通</p>
         <p>支付方式：<span>支付宝</span></p>
